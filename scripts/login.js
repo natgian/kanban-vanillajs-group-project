5e20d0c8704e9d20ecb4a112a85934ceba67ad02
Wladislav Bender <wladislav.bender@gmail.com>
2025-05-03 19:06:27 +0200

    added JSDoc Comment & Clean Code

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,11 @@
+/**
+ * This function is required to start the page and to load essential functions.
+ */
 function init() {
     let loader = document.getElementById('loader');
     let logo = document.getElementById('logo');
     logo.classList.add('fly');
     setTimeout(() => {
-      loader.classList.add('hidden');
+        loader.classList.add('hidden');
     }, 1500);
-  }
-  
+}
