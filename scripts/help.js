19bb0911e05b9a1c207c9fb01ba4143aa3895e32
Wladislav Bender <wladislav.bender@gmail.com>
2025-05-07 17:54:00 +0200

    added JSDoc comment

--- scripts/help.js
+++ scripts/help.js
@@ -1,3 +1,9 @@
+/**
+ * Navigates the browser to the previous page in the session history.
+ *
+ * This function mimics the behavior of the browser's back button.
+ * If there is no previous page in the history, the function does nothing.
+ */
 function goBack() {
     window.history.back();
 }
