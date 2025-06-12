eba4075ca4d70a194be2e74f75542d2b566d6996
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-06-12 18:00:51 +0200

    Refactored messageBox code to shorten the main function (max 14 lines). Added goBack function to main script and removed help.js file

--- scripts/help.js
+++
@@ -1,9 +0,0 @@
-/**
- * Navigates the browser to the previous page in the session history.
- *
- * This function mimics the behavior of the browser's back button.
- * If there is no previous page in the history, the function does nothing.
- */
-function goBack() {
-    window.history.back();
-}
