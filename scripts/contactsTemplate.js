f8ef0329e7a5f6b7773b3514d90bfd38d26995e4
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-06-20 20:40:18 +0200

    Added media queries for contacts

--- scripts/contactsTemplate.js
+++ scripts/contactsTemplate.js
@@ -14,7 +14,7 @@ function templateContacts(contact) {
   </div>`;
 }
 
-function templateContactsDetails({id, name, email, monogram, phone, monogramColor }) {
+function templateContactsDetails({ id, name, email, monogram, phone, monogramColor }) {
   return `
     <div class="contact-details-wrapper">
       <div class="content-top">
@@ -71,7 +71,7 @@ function templateContactsDetails({id, name, email, monogram, phone, monogramColo
   `;
 }
 
-function templateNewContact(){
+function templateNewContact() {
   return `
       <div id="popup">
         <div id="popupCloseBtn" class="popup-close">
@@ -106,10 +106,10 @@ function templateNewContact(){
             </div>
           </form>
         </div>
-      </div>`
+      </div>`;
 }
 
-function templateEditContact({id, name, email, monogram, phone, monogramColor }){
+function templateEditContact({ id, name, email, monogram, phone, monogramColor }) {
   return `
       <div id="popup" class="active-contact">
         <div id="popupCloseBtn" class="popup-close">
@@ -143,75 +143,5 @@ function templateEditContact({id, name, email, monogram, phone, monogramColor })
             </div>
           </form>
         </div>
-      </div>`
+      </div>`;
 }
-/*
-function templateNewContactMobile(){
-  return `<div class="popupMobile">
-            <div onclick="closeMobilePopUp()" class="popup-close">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-            </div>
-            <div class="popupMobileTop">
-              <h1>Add contact</h1>
-              <p>Tasks are better with a team!</p>
-              <div class="divider-horizontal"></div>
-            </div>
-            <div class="profile-logo">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-            </div>
-            <div class="popup-bottom">
-            <form onsubmit="createNewContact(event)">
-              <div class="input-container">
-                <input required type="text" id="name" placeholder="Name">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-              </div>
-              <div class="input-container">
-                <input required type="email" id="email" placeholder="Email">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
-              </div>
-              <div class="input-container">
-                <input required type="tel" id="phone" placeholder="Phone">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg>
-              </div>
-              <div class="create-buttons-div">
-                <button type="submit">Create Contact<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></button>
-              </div>
-            </form>
-          </div>
-        </div>`
-}
-
-function templateEdtContactMobile({id, name, email, monogram, phone, monogramColor }){
-  return `<div class="popupMobile">
-            <div onclick="closeMobilePopUp()" class="popup-close">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-            </div>
-            <div class="popupMobileTop">
-              <h1>Edit contact</h1>
-              <div class="divider-horizontal"></div>
-            </div>
-            <div class="profile-logo contact-monogram monogram" style="transform: translate(-50%, -65%); background-color: ${monogramColor};">
-              ${monogram}
-            </div>
-            <div class="popup-bottom">
-            <form onsubmit="updateContact(event, '${id}')">
-              <div class="input-container">
-                <input required type="text" id="name" value="${name}">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-              </div>
-              <div class="input-container">
-                <input required type="email" id="email" value="${email}">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
-              </div>
-              <div class="input-container">
-                <input required type="tel" id="phone" value="${phone}">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg>
-              </div>
-              <div class="create-buttons-div">
-                <button type="reset" onclick="deleteContact(event, '${id}')">Delete</button>
-                <button type="submit">Save<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></button>
-              </div>
-            </form>
-          </div>
-        </div>`
-}*/
